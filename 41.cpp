0510083712ec0624c9357dcae9a1a1ad23925c42
Sevy <568898064@qq.com>
2016-11-24 19:36:22 +0800

    41 2016.11.24

---
+++ 41.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int> A) {
+        int n = A.size();
+        int i = 0;
+        while (i < n)
+        {
+            if (A[i] != (i+1) && A[i] >= 1 && A[i] <= n && A[A[i]-1] != A[i])
+                swap(A[i], A[A[i]-1]);
+            else
+                i++;
+        }
+        for (i = 0; i < n; ++i)
+            if (A[i] != (i+1))
+                return i+1;
+        return n+1;
+    }
+};
