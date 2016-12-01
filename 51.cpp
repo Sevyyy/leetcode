b2b86136d1cd3cb7efbc5c1d34e449245d56687d
Sevy <568898064@qq.com>
2016-12-01 13:55:42 +0800

    51 2016.12.1

---
+++ 51.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    void DFS(vector<vector<string>> &ans, vector<string> &temp, int* used1, int* used2, int* used3, int n)
+    {
+        if(temp.size() == n)
+        {
+            ans.push_back(temp);
+            return;
+        }
+        else
+        {
+            int r = temp.size();
+            for(int i = 0;i < n;i++)
+            {
+                if(!used1[i] && !used2[r+i] && !used3[i-r+n-1])
+                {
+                    used1[i] = 1;
+                    used2[r+i] = 1;
+                    used3[i-r+n-1] = 1;
+                    string row = "";
+                    for(int j = 0;j<n;j++)
+                        row = row+'.';
+                    row[i] = 'Q';
+                    temp.push_back(row);
+                    DFS(ans, temp, used1, used2, used3, n);
+                    used1[i] = 0;
+                    used2[r+i] = 0;
+                    used3[i-r+n-1] = 0;
+                    temp.pop_back();
+                }
+            }
+        }
+        return;
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        if(!n)
+            return ans;
+        int *used1 = new int[n];
+        int *used2 = new int[2*n-1];
+        int *used3 = new int[2*n-1];
+        for(int i = 0;i < n;i++)
+            used1[i] = 0;
+        for(int i = 0;i < 2*n-1;i++)
+            used2[i] = 0;
+        for(int i = 0;i < 2*n-1;i++)
+            used3[i] = 0;
+        vector<string> temp;
+        DFS(ans, temp, used1, used2, used3, n);
+        return ans;
+    }
+};
