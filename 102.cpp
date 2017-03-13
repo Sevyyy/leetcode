f8761660fd95e00989c6bf3652bc389f66189064
Sevy <568898064@qq.com>
2017-03-13 13:49:13 +0800

    102 2017.3.13

---
+++ 102.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if(!root)
+            return ans;
+        vector<TreeNode*> q;
+        q.push_back(root);
+        vector<int> t;
+        t.push_back(root->val);
+        ans.push_back(t);
+        int index = 0;
+        int len = 1;
+        while(index < q.size())
+        {
+            vector<int> temp;
+            int l = 0;
+            for(int i = index; i < index+len; i++)
+            {
+                if(q[i]->left)
+                {
+                    q.push_back(q[i]->left);
+                    temp.push_back(q[i]->left->val);
+                    l++;
+                }
+                if(q[i]->right)
+                {
+                    q.push_back(q[i]->right);
+                    temp.push_back(q[i]->right->val);
+                    l++;
+                }
+            }
+            if(temp.size())
+                ans.push_back(temp);
+            index += len;
+            len = l;
+        }
+        return ans;
+    }
+};
